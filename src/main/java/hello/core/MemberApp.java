ebe4166d8165f4808e9ce922af2232c3423bfdb0
joonghyun <joongh@ewhain.net>
2022-05-17 16:51:50 +0900

    Sec 2-5_회원 도메인 실행과 테스트

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,10 +7,12 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
+        //new member 생성 및 join
         MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
+        //Test
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
